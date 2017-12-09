a929ef4139a6c304dafa6582f6431546ca8bef0d
Sergey <sunliveua@gmail.com>
2017-12-09 21:18:51 +0200

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const productRoutes = require('./api/routes/products');
@@ -8,10 +9,23 @@ const orderRoutes = require('./api/routes/orders');
 // using morgan package for logging
 app.use(morgan('dev'));
 
-
 // using bodyParser included in express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// add cors
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'make it better'
+git push
