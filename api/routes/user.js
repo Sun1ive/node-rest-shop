393bed14482e4dbd2683cf4408dff24d0c5c9d7f
Sergey <sunliveua@gmail.com>
2018-01-07 18:18:41 +0200

    💤 💤

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const UserController = require('../controllers/user');
+const checkAuth = require('../middleware/check-auth')
 
 router.post('/signup', UserController.user_sign_up);
 
 router.post('/login', UserController.user_sign_in);
 
-router.delete('/:userId', UserController.user_delete);
+router.delete('/:userId', checkAuth, UserController.user_delete);
 
 module.exports = router;
